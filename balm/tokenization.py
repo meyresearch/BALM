19f162f5ff0888d4ee461315a93e27f789fe9636
rgorantla04 <rohan.gorantla@ed.ac.uk>
2024-08-14 14:28:51 +0100

    updated tokenizers

--- balm/tokenization.py
+++ balm/tokenization.py
@@ -7,6 +7,17 @@ def pre_tokenize_unique_entities(
     protein_tokenizer: PreTrainedTokenizer,
     drug_tokenizer: PreTrainedTokenizer,
 ):
+    """
+    Pre-tokenizes unique protein sequences and ligand SMILES in the dataset.
+
+    Args:
+        dataset (pd.DataFrame): A DataFrame containing the dataset with 'Target' and 'Drug' columns.
+        protein_tokenizer (PreTrainedTokenizer): Tokenizer for protein sequences.
+        drug_tokenizer (PreTrainedTokenizer): Tokenizer for drug sequences.
+
+    Returns:
+        tuple: Two dictionaries containing the tokenized input IDs and attention masks for proteins and drugs.
+    """
     unique_proteins = dataset["Target"].unique().tolist()
     unique_drugs = dataset["Drug"].unique().tolist()
 
@@ -23,12 +34,10 @@ def pre_tokenize_unique_entities(
         )
     }
 
-    # Check if there's non string values in unique_drugs
-    # Count how many unique drugs are not strings
+    # Check if there are non-string values in the unique drug list
     non_string_drugs = sum([not isinstance(drug, str) for drug in unique_drugs])
     if non_string_drugs > 0:
-        # Print the non string drugs
-        print("Non string drugs:")
+        print("Non-string drugs:")
         print([drug for drug in unique_drugs if not isinstance(drug, str)])
 
     tokenized_drugs = drug_tokenizer(unique_drugs)
@@ -48,6 +57,17 @@ def pre_tokenize_unique_entities(
 
 
 def tokenize_with_lookup(examples, protein_tokenized_dict, drug_tokenized_dict):
+    """
+    Retrieves pre-tokenized protein and drug sequences using lookup dictionaries.
+
+    Args:
+        examples (dict): A dictionary containing the 'Target' and 'Drug' keys for sequences.
+        protein_tokenized_dict (dict): Dictionary with pre-tokenized protein sequences.
+        drug_tokenized_dict (dict): Dictionary with pre-tokenized drug sequences.
+
+    Returns:
+        dict: A dictionary with the original sequences, tokenized input IDs, and attention masks.
+    """
     protein_input = protein_tokenized_dict[examples["Target"]]
     drug_input = drug_tokenized_dict[examples["Drug"]]
 
