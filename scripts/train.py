dae7135ec9e9bdc86d232b43d4ae5620800cbc40
aryopg@gmail.com <aryopg@gmail.com>
2024-10-30 16:46:02 +0000

    Adding inline documentations

--- scripts/train.py
+++ scripts/train.py
@@ -24,20 +24,31 @@ def main() -> None:
     args = argument_parser()
     configs = Configs(**common_utils.load_yaml(args.config_filepath))
 
+    # Set the random seed for reproducibility
     common_utils.setup_random_seed(configs.training_configs.random_seed)
+    
+    # Setup the output directory for the experiment
     outputs_dir = common_utils.setup_experiment_folder(
         os.path.join(os.getcwd(), configs.training_configs.outputs_dir)
     )
+    
+    # Save the training configuration to the output directory
     common_utils.save_training_configs(configs, outputs_dir)
 
-    # Login to WandB
+    # Login to Weights & Biases (WandB)
     wandb_entity = os.getenv("WANDB_ENTITY", "")
     wandb_project = os.getenv("WANDB_PROJECT_NAME", "")
 
+    # Initialize the Trainer and start training
     trainer = Trainer(configs, wandb_entity, wandb_project, outputs_dir)
-    # train_ratio is only useful for Mpro, the rest will be null
+    
+    # Set the dataset with an optional train ratio (specific to Mpro)
     trainer.set_dataset(train_ratio=configs.dataset_configs.train_ratio)
+    
+    # Setup the training environment
     trainer.setup_training()
+    
+    # Start the training loop
     trainer.train()
 
 
